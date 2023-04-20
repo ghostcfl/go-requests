0a33d6fc2bbfdf011ed9203e889fe711623df763
蔡福良 <949930866@qq.com>
2023-04-20 11:55:39 +0800

    init

--- main.go
+++ main.go
@@ -3,59 +3,82 @@ package main
 import (
 	"fmt"
 	"github.com/ghostcfl/go-requests/requests"
-	"sync"
+	"os"
 )
 
-type KaWaYiYiTls struct {
-	Sni             string   `json:"sni"`
-	TlsVersion      string   `json:"tlsVersion"`
-	TcpConnectionId string   `json:"tcpConnectionId"`
-	TlsHashOrigin   string   `json:"tlsHashOrigin"`
-	TlsHashMd5      string   `json:"tlsHashMd5"`
-	CipherList      []string `json:"cipherList"`
-	Extentions      []string `json:"extentions"`
-	Supportedgroups []string `json:"supportedgroups"`
-	EcPointFormats  []string `json:"ecPointFormats"`
-	Proto           string   `json:"proto"`
-	H2              struct {
-		SETTINGS struct {
-			Field1 string `json:"2"`
-			Field2 string `json:"4"`
-			Field3 string `json:"6"`
-		} `json:"SETTINGS"`
-		WINDOWUPDATE string   `json:"WINDOW_UPDATE"`
-		HEADERS      []string `json:"HEADERS"`
-	} `json:"h2"`
-	UserAgent string `json:"user_agent"`
-	ClientIp  string `json:"clientIp"`
-}
-
-func ja3_check(name string) {
-	resp, err := requests.Get("https://kawayiyi.com/tls", requests.GP{})
+func postFormAndFiles() {
+	file, err := os.ReadFile("1.txt")
 	if err != nil {
-		panic(err)
+		return
 	}
-	var data KaWaYiYiTls
-	err = resp.Json(&data)
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Files: &requests.Files{
+			"file1": requests.F{
+				Filename: "hello.txt",
+				Buffer:   file,
+			},
+		},
+		Form: &requests.KV{
+			"username": "caifuliang",
+		},
+	})
 	if err != nil {
 		return
 	}
 
-	fmt.Printf("%s请求的TlsHashMd5:%s\t%s请求的Proto:%s\n", name, data.TlsHashMd5, name, data.Proto)
+	fmt.Println(resp.Text())
+}
+
+func postJson() {
+	// use J struct
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Json: &requests.J{
+			"a": "b",
+			"b": []string{"1", "2", "3"},
+			"c": requests.KV{
+				"c1":  "val c1",
+				"c22": "val c2",
+			},
+		},
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Text())
+	// use JsonString
+	resp, err = requests.Post("https://httpbin.org/post", requests.PP{
+		JsonString: `{"a":"b","b":["1","2","3"],"c":{"c1":"val c1","c22":"val c2"}}`,
+	})
+	if err != nil {
+		return
+	}
+
+	fmt.Println(resp.Text())
 
 }
 
-func main() {
-	var wg sync.WaitGroup
-	{
+func postUrlencoded() {
+	// use KV struct
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Data: &requests.KV{
+			"a":    "b",
+			"name": "caifuliang",
+		},
+	})
+	if err != nil {
+		return
 	}
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(i int) {
-			defer wg.Done()
-			ja3_check(fmt.Sprintf("协程%d", i))
-		}(i)
+	fmt.Println(resp.Text())
+	// use urlencoded string
+	resp, err = requests.Post("https://httpbin.org/post", requests.PP{
+		DataString: "a=b&name=caifuliang",
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Text())
+}
 
-	}
-	wg.Wait()
+func main() {
+	postUrlencoded()
 }
--- readme.md
+++ readme.md
@@ -111,3 +111,91 @@ func main() {
 协程0请求的TlsHashMd5:907ddc4b40855e3e820900940c1d539d	协程0请求的Proto:HTTP/2
 协程2请求的TlsHashMd5:1e6ce23b75d203e2880b6c35a6039aef	协程2请求的Proto:HTTP/2
 ```
+### (五)post请求
+```go
+package main
+
+import (
+	"fmt"
+	"github.com/ghostcfl/go-requests/requests"
+	"os"
+)
+
+func postFormAndFiles() {
+	file, err := os.ReadFile("1.txt")
+	if err != nil {
+		return
+	}
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Files: &requests.Files{
+			"file1": requests.F{
+				Filename: "hello.txt",
+				Buffer:   file,
+			},
+		},
+		Form: &requests.KV{
+			"username": "caifuliang",
+		},
+	})
+	if err != nil {
+		return
+	}
+
+	fmt.Println(resp.Text())
+}
+
+func postJson() {
+	// use J struct
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Json: &requests.J{
+			"a": "b",
+			"b": []string{"1", "2", "3"},
+			"c": requests.KV{
+				"c1":  "val c1",
+				"c22": "val c2",
+			},
+		},
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Text())
+	// use JsonString
+	resp, err = requests.Post("https://httpbin.org/post", requests.PP{
+		JsonString: `{"a":"b","b":["1","2","3"],"c":{"c1":"val c1","c22":"val c2"}}`,
+	})
+	if err != nil {
+		return
+	}
+
+	fmt.Println(resp.Text())
+
+}
+
+func postUrlencoded() {
+	// use KV struct
+	resp, err := requests.Post("https://httpbin.org/post", requests.PP{
+		Data: &requests.KV{
+			"a":    "b",
+			"name": "caifuliang",
+		},
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Text())
+	// use urlencoded string
+	resp, err = requests.Post("https://httpbin.org/post", requests.PP{
+		DataString: "a=b&name=caifuliang",
+	})
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Text())
+}
+
+func main() {
+	postUrlencoded()
+}
+
+```
